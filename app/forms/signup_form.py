ccdf9e6f51c90ecb34ce19e5ca34f4238caf5bdd
Rob Whitacre <robert.w.whitacre@gmail.com>
2021-03-20 13:19:43 -0400

    test

--- app/forms/signup_form.py
+++ app/forms/signup_form.py
@@ -16,3 +16,5 @@ class SignUpForm(FlaskForm):
     username = StringField('username', validators=[DataRequired()])
     email = StringField('email', validators=[DataRequired(), user_exists])
     password = StringField('password', validators=[DataRequired()])
+
+## test test
