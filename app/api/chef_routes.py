6ecf8eab21d9a39665df55d468b2df7246d842f0
Ace Habes <72156174+Ace-0101@users.noreply.github.com>
2021-03-24 08:24:19 -0700

    notes-for-search

--- app/api/chef_routes.py
+++ app/api/chef_routes.py
@@ -13,7 +13,21 @@ chef_routes = Blueprint('chefs', __name__)
 def get_all_chefs():
     users = User.query.all()
     return {'chefs': [user.to_dict() for user in users if user.chef_id]}
+   
 
+
+#TODO this is going to fitch the data for the chef where the chef have an 'h' anywhere.
+#! PS you can set a backTek `` inside the .like and set a variable that will change while typing
+#! depends on what you type!
+#! also you may be able to do ilike instead for case sensiteve
+
+#  users = User.query.filter(User.first_name.like('h%'))
+#     return {'chefs': [user.to_dict() for user in users if user.chef_id]}
+#TODO                                                                             
+#TODO you can grab the id of that chef and display it after you fetch it maybe we need to use the
+#TODO the store instead?! and then loop over all the chefs and display it one by one.
+#TODO since we are looping through it we can get the id and onclick navigate to the chef profile.
+#! if we got it to work we can do a settimeout that will show the chefs only after x amount of milliseconds.
 
 
 # @chef_routes.route('/', methods=['POST'])
--- react-app/src/App.js
+++ react-app/src/App.js
@@ -3,7 +3,7 @@ import { Route, Switch, Redirect } from "react-router-dom";
 import LoginForm from "./components/auth/LoginForm";
 import SignUpForm from "./components/auth/SignUpForm";
 import NavBar from "./components/NavBar";
-import ProtectedRoute from "./components/auth/ProtectedRoute";
+// import ProtectedRoute from "./components/auth/ProtectedRoute";
 import UsersList from "./components/UsersList";
 import User from "./components/User";
 import HomePage from "./components/HomePage";
@@ -11,44 +11,56 @@ import { useDispatch, useSelector } from "react-redux";
 import { restoreUser } from "./store/session";
 
 function App() {
-  const dispatch = useDispatch()
-  const [loaded, setLoaded] = useState(false);
-  const user = useSelector((state) => state.session.user)
+    const dispatch = useDispatch()
+    const [loaded, setLoaded] = useState(false);
+    const user = useSelector((state) => state.session.user)
 
-  useEffect(async () => {
-    await dispatch(restoreUser())
-    setLoaded(true)
-  }, [dispatch])
+    useEffect(async() => {
+        await dispatch(restoreUser())
+        setLoaded(true)
+    }, [dispatch])
 
-  if (!loaded) {
-    return null;
-  }
+    if (!loaded) {
+        return null;
+    }
 
-  if (!user) {
-    //THIS IS CAUSING ISSUES
-    // return <Redirect to='/login' />
-  }
+    if (!user) {
+        //THIS IS CAUSING ISSUES
+        // return <Redirect to='/login' />
+    }
 
-  return (
-    <>
-      <NavBar />
-      {loaded && (
-        <Switch>
-          <Route path="/login" exact={true}>
-            <LoginForm
-            />
-          </Route>
-          <Route path="/sign-up" exact={true}>
-            <SignUpForm />
-          </Route>
-          <Route path="/" exact={true} >
-            <HomePage />
-          </Route>
-        </Switch>
-      )}
-    </>
+    return ( <
+        >
+        <
+        NavBar / > {
+            loaded && ( <
+                Switch >
+                <
+                Route path = "/login"
+                exact = { true } >
+                <
+                LoginForm /
+                >
+                <
+                /Route> <
+                Route path = "/sign-up"
+                exact = { true } >
+                <
+                SignUpForm / >
+                <
+                /Route> <
+                Route path = "/"
+                exact = { true } >
+                <
+                HomePage / >
+                <
+                /Route> <
+                /Switch>
+            )
+        } <
+        />
 
-  );
+    );
 }
 
 export default App;
