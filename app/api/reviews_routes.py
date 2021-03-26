644381ebd30c7b8f440c4d741d2ae241b4d6a1aa
zero-cool94 <emadmasoud2017@hotmail.com>
2021-03-26 11:50:56 -0500

    finish mergeing main into branch

--- app/api/reviews_routes.py
+++ app/api/reviews_routes.py
@@ -5,19 +5,13 @@ reviews_routes = Blueprint('reviews', __name__)
 
 
 @reviews_routes.route('/chef/<int:id>/')
-<<<<<<< HEAD
-def reviews(id):
+def chef_reviews(id):
     reviews = Review.query.filter_by(chef_id=id).all()
     return jsonify({"reviews": [review.to_dict() for review in reviews]})
-=======
-def chef_reviews(id):
-  reviews=Review.query.filter_by(chef_id=id).all()
-  return jsonify({"reviews": [review.to_dict() for review in reviews]})
+
 
 @reviews_routes.route('/<int:id>/')
 def user_reviews(id):
-  reviews=Review.query.filter_by(user_id=id).all()
-  print("REVIEWS FOR USER---->>>>", reviews)
-  return jsonify({"reviews": [review.to_dict() for review in reviews]})
-
->>>>>>> main
+    reviews = Review.query.filter_by(user_id=id).all()
+    print("REVIEWS FOR USER---->>>>", reviews)
+    return jsonify({"reviews": [review.to_dict() for review in reviews]})
